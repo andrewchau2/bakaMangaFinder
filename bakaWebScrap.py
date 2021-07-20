bee4bf0d6880aa07f4b70b8ba03978fd66dda947
Andrew Chau <xfriendsx1@gmail.com>
2021-07-20 13:59:25 -0500

    Finished Webscrapper for baka-updates manga

--- bakaWebScrap.py
+++ bakaWebScrap.py
@@ -6,9 +6,13 @@ class baka(object):
 
     def __init__(self):
         self.all_genres = ['action', 'adult', 'adventure', 'comedy', 'doujinshi', 'drama', 'ecchi', 'fantasy', 'gender bender', 'harem', 'hentai', 'historical', 'horror', 'josei', 'lolicon', 'martial arts', 'mature', 'mecha', 'mystery', 'psychological', 'romance', 'school life', 'sci-fi', 'seinen', 'shotacon', 'shoujo', 'shoujo ai', 'shounen', 'shounen ai', 'slice of life', 'smut', 'sports', 'supernatural', 'tragedy', 'yaoi', 'yuri']
-
         self.include_tags = []
         self.exclude_tags = []
+        self.search_options = {'perpage' : None , 'orderby' : None}
+
+    def reset_search_options(self):
+        self.search_options['perpage'] = None
+        self.search_options['orderby'] = None
 
     def reset_include_tag(self):
         self.include_tags = []
@@ -16,35 +20,68 @@ class baka(object):
     def reset_exclude_tag(self):
         self.exclude_tags = []
 
+    # Allows the user to change their search options. If this isn't called, the default search option is used
+    def setSearch_options(self, perpage=-1, orderby=''):
+        perpageOptions = [5,10,15,25,30,40,50]
+        orderbyOptions = ['title','year','rating']
+        assert orderby is str
+        assert perpage is int and perpage > 0
+        if orderby != '' and orderby.lower() in orderbyOptions:
+            self.search_options['orderby'] = orderby.lower()
+        if perpage != -1 and perpage in perpageOptions:
+            self.search_options['perpage'] = perpage
+
+    # Allows the user to add include tags to their search preference
     def setInclude_tags(self, lst = []):
         if len(lst) == 0:
             return
         for genre in lst:
-            if genre.lower() in self.all_genres:
+            if genre.lower() in self.all_genres and genre.lower() not in self.include_tags:
                 tmp = genre.split(" ")
                 toAdd = ""
                 for words in tmp:
                     toAdd += words.capitalize() + " "
                 toAdd = toAdd[:-1]
                 self.include_tags.append(toAdd)
+
+    #Allows the user to add exclude tags to their search preference
     def setExclude_tags(self,lst = []):
         if len(lst) == 0:
             return
         for genre in lst:
-            if genre.lower() in self.all_genres:
+            if genre.lower() in self.all_genres and genre.lower() not in self.exclude_tags:
                 tmp = genre.split(" ")
                 toAdd = ""
                 for words in tmp:
                     toAdd += words.capitalize() + " "
                 toAdd = toAdd[:-1]
                 self.exclude_tags.append(toAdd)
-    def setSearchInfo(self,page=-1):
+
+
+    #Sets the search info based on user preference
+    def setSearchInfo(self,page=-1, perpage = -1, orderby = None):
+
+        assert page,perpage is not int
         base_url = "https://www.mangaupdates.com/series.html?"
+
+        setPerPage = ""
+        if perpage != -1 and perpage > 0:
+            setPerPage = '&perpage=' + str(perpage)
+        else:
+            setPerPage = '&perpage=5'
         setPage = ""
         if page != -1 and page > 0:
             setPage = "page=" + str(page)
+
+        setOrderby  = ''
+        if orderby is not None:
+            setOrderby = '&orderby=' + orderby;
+        else:
+            setOrderby = '&orderby=rating'
+
         exclude = ""
         include = ""
+
         if len(self.include_tags) != 0:
             include = "&genre="
             for i in self.include_tags:
@@ -56,19 +93,24 @@ class baka(object):
             for j in self.exclude_tags:
                 exclude += j.replace(" ","+") + "_"
             exclude = exclude[:-1]
-        addon = setPage + "&orderby=rating&perpage=5" + include + exclude
+        addon = setPage + setOrderby + setPerPage + include + exclude
+
         return base_url + addon
 
+    #Intended for search_by_tags() function. It lets the user know how much pages the
+    #webscrap can go to
     def getPageNumber(self):
         search_url = self.setSearchInfo()
         html_doc = requests.get(search_url).text
         soup = BeautifulSoup(html_doc,'lxml')
         totalPages = soup.find(name="span",class_="d-none d-md-inline-block")
-        if totalPages == None:
+        if totalPages is None:
             return None
         else:
             return int(totalPages.text.split(" ")[1][1:-1])
 
+    # Calls setSearchInfo to set the info based on your tags and then
+    # searches a random page based on your pref
     def random_search_by_tag(self):
         pageLimit =self.getPageNumber()
         if pageLimit is None:
@@ -76,8 +118,11 @@ class baka(object):
             return None
         else:
             randNum = random.randrange(1, pageLimit)
-            print("Random number:",randNum)
             return self.search_by_tags(page=randNum)
+
+
+    #Calls setSearchInfo to set the info based on your tags and then
+    #searches based on page number given. If page = -1, then first page is scrapped
     def search_by_tags(self,page = -1):
         search_url = ""
         assert page is not int
@@ -96,11 +141,8 @@ class baka(object):
         for i in mangasInfo:
             newDic = {}
             newDic['title'] = i.find(name="b").text
-            #print(i.find(name="b").text)
             newDic['desc'] = i.find(name="div",class_="text flex-grow-1").text
-            #print(i.find(name="div",class_="text flex-grow-1").text)
             newDic['genres'] = i.find(name="div",class_="textsmall").text
-            #print(i.find(name="div",class_="textsmall").text)
             web = i.find(name='a',href=True)
             newDic['web'] = web['href']
             imgT = i.find(name='img',src=True)
@@ -112,7 +154,8 @@ class baka(object):
             count = 0
             for j in rating:
                 if count == 3:
-                    #print(j.text)
+                    if j is None:
+                        newDic['score'] = None
                     newDic['score'] = j.text
                     break
                 count += 1
@@ -121,6 +164,8 @@ class baka(object):
         return mangaCollection
 
     def getCategories(self):
+    #This isn't called, but it provides a way to obtain all the categories.
+    #Categories are provided in a text file called genres.txt
 
         htmlF = requests.get('https://www.mangaupdates.com/genres.html').text
         soup = BeautifulSoup(htmlF,'lxml')
@@ -128,7 +173,6 @@ class baka(object):
         genresTitles = genresParent.find_all(name="div", class_="pl-3 pt-3 pr-3 releasestitle")
         with open('genres.txt','w') as ifstrm:
             for title in genresTitles:
-               # print(title.text.lower())
                 self.all_genres.append(title.text.lower())
                 ifstrm.write(title.text.lower() + '\n')
             ifstrm.close()
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import bakaWebScrap as bak
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    lst = ['gender bender']
-    #lst = ['horror','lolicon','mature','mystery','romance','sci-fi']
-    #lst = ['gender bender', 'horror', 'ecchi']
+
+    includeTags = ['action', 'adventure'] #Add include tags here
+    excludeTags = ['slice of life'] #Add exclude tags here
     obj = bak.baka()
-    # obj.getCategories()
-    obj.setInclude_tags(lst=lst)
-    #tmp = obj.search_by_tags()
-    tmp = obj.random_search_by_tag()
-    for i in range(len(tmp)):
-        print(i, ":")
-        for j in tmp[i].values():
-            print(j)
-        print("\n")
+    obj.setInclude_tags(lst=includeTags)
+    obj.setExclude_tags(lst=excludeTags)
+
+    # Optional. Default search settings is used if we don't set anything
+    #obj.setSearch_options(perpage=5,orderby='rating')
+
+
+    print("Page number: ", obj.getPageNumber()) #Gets the total page number for your search
+    results = obj.search_by_tags()
+    print(results)
+
+    #results = obj.random_search_by_tag() #There is also a random search option based on your pref
+
